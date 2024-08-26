85c6ed14c59137da4f1737c76ca0192102d84043
ReadOne <muh.ridwan97@gmail.com>
2024-08-26 21:13:04 +0700

    fix hostname

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
+	'hostname' => '172.18.0.1',
 	'username' => 'cakra-global',
 	'password' => 'Cakraglobal888',
 	'database' => 'cakra-global',
