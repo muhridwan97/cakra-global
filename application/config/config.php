651c94e30151f34b6fdd69af32e8bf0f5ef139d0
ReadOne <muh.ridwan97@gmail.com>
2024-08-26 22:24:52 +0700

    pakek domain

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://172.105.120.116:8080';
+$config['base_url'] = 'https://cakralogistic.co.id';
 
 /*
 |--------------------------------------------------------------------------
