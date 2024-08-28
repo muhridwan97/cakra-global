518bdad8d78034d1be5c0b7ceb12dc15f629abc4
ReadOne <muh.ridwan97@gmail.com>
2024-08-28 08:57:58 +0700

    tambah https

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://cakralogistic.co.id';
+$config['base_url'] = 'https://cakralogistic.co.id';
 
 /*
 |--------------------------------------------------------------------------
