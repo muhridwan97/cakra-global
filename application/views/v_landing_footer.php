4b1b083e89565f72ce60b6ebd2b2dbe471865e95
ReadOne <muh.ridwan97@gmail.com>
2024-08-29 09:59:22 +0700

    fix footer

---
+++ application/views/v_landing_footer.php
@@ -0,0 +1,54 @@
+<!-- Footer Start -->
+<div class="container-fluid bg-dark text-light footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-4">Alamat</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Jl. Kebon Bawang VIII No.20</p>
+                    <p style="text-align: justify;">RT.19/RW.1, Kb. Bawang, Kec. Tj. Priok, Jakarta Utara</p>
+                    <div class="d-flex pt-2">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-4">Link</h4>
+                    <a class="btn btn-link" href="<?php echo base_url(); ?>#tentang-kami">Tentang Kami</a>
+                    <a class="btn btn-link" href="<?php echo base_url(); ?>c_web/artikel">Artikel</a>
+                    <a class="btn btn-link" href="<?php echo base_url(); ?>c_web/hubungiKami">Hubungi Kami</a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-4">Layanan Kami<h4>
+                    <a class="btn btn-link" href="<?php echo base_url(); ?>c_web/layananKami/freight_forwarding">Freight Forwarding</a>
+                    <a class="btn btn-link" href="<?php echo base_url(); ?>c_web/layananKami/custome_handling">Custom Handling</a>
+                    <a class="btn btn-link" href="<?php echo base_url(); ?>c_web/layananKami/c_web/warehouse">Warehouse</a>
+                    <a class="btn btn-link" href="<?php echo base_url(); ?>c_web/layananKami/project_logistic">Project Logistic</a>
+                    <a class="btn btn-link" href="<?php echo base_url(); ?>c_web/layananKami/trucking">Trucking</a>
+                    <a class="btn btn-link" href="<?php echo base_url(); ?>c_web/layananKami/plb">PLB</a>
+
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-4">Informasi</h4>
+                    <p class="mb-2"><i class="fa fa-globe me-3"></i><a href="">cakralogistic.co.id</a></p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>axel.rizky@cakralogistic.co.id</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+62-2121-6977-04</p>
+                    <div class="position-relative w-100">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Copyright Start -->
+    <div class="container-fluid copyright py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a class="border-bottom" href="<?php echo base_url(); ?>">Cakra Global Logistik</a>
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    Website by : RR DESIGN
+                </div>
+            </div>
+        </div>
+    </div>
